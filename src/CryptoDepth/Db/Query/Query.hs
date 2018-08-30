d22a5e83483d053a41a50c77afcd2d7d44ad4607
Rune K. Svendsen <runesvend@gmail.com>
2018-08-30 14:48:37 +0200

    Query: fix build failure

--- src/CryptoDepth/Db/Query/Query.hs
+++ src/CryptoDepth/Db/Query/Query.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 module CryptoDepth.Db.Query.Query
-(
+( newestPathsQ
 )
 where
 
@@ -14,33 +14,45 @@ import Database.Beam.Postgres
 import Database.Beam.Postgres.Syntax        (PgExpressionSyntax)
 
 
-allRuns
-    :: Q PgSelectSyntax CryptoDepthDb s (RunT (QExpr PgExpressionSyntax s))
-allRuns = all_ (_runInfo cryptoDepthDb)
 
-selectSortedRuns
-    :: SqlSelect PgSelectSyntax Run
-selectSortedRuns =
-    select sortBy
-  where
-    sortBy = orderBy_ (desc_ . _runId) allRuns
-
-newestRun =
-    limit_ 1 selectSortedRuns
+newestPathsQ
+    :: (KnownSymbol numeraire, PathTable numeraire Postgres)
+    => Q PgSelectSyntax CryptoDepthDb s (PathT numeraire (QExpr PgExpressionSyntax s))
+newestPathsQ = do
+    run <- newestRunQ
+    path <- allPathsQ
+    guard_ (_pathRunId path ==. pk run)
+    return path
 
 
-allPaths
-    :: forall numeraire s.
-       (KnownSymbol numeraire, PathTable numeraire Postgres)
-    => Q PgSelectSyntax CryptoDepthDb s ((PathT numeraire) (QExpr PgExpressionSyntax s))
-allPaths = all_ (pathTable cryptoDepthDb :: PathEntityType Postgres numeraire)
-
-usdPaths
+pathList
     :: (KnownSymbol numeraire, PathTable numeraire Postgres)
     => Pg [Path numeraire]
-usdPaths = runSelectReturningList selectAllPaths
+pathList = runSelectReturningList selectAllPaths
 
 selectAllPaths
     :: (KnownSymbol numeraire, PathTable numeraire Postgres)
     => SqlSelect PgSelectSyntax (QExprToIdentity (PathT numeraire (QExpr PgSelectSyntax s)))
-selectAllPaths = select allPaths
+selectAllPaths = select allPathsQ
+
+allPathsQ
+    :: forall numeraire s.
+       (KnownSymbol numeraire, PathTable numeraire Postgres)
+    => Q PgSelectSyntax CryptoDepthDb s ((PathT numeraire) (QExpr PgExpressionSyntax s))
+allPathsQ = all_ (pathTable cryptoDepthDb :: PathEntityType Postgres numeraire)
+
+
+newestRun :: Pg (Maybe Run)
+newestRun = runSelectReturningOne $ select newestRunQ
+
+newestRunQ :: Q PgSelectSyntax CryptoDepthDb s (RunT (QGenExpr QValueContext PgExpressionSyntax s))
+newestRunQ =
+    limit_ 1 sortedRuns
+
+sortedRuns :: Q PgSelectSyntax CryptoDepthDb s (RunT (QGenExpr QValueContext PgExpressionSyntax s))
+sortedRuns =
+    orderBy_ (desc_ . _runId) allRuns
+
+allRuns
+    :: Q PgSelectSyntax CryptoDepthDb s (RunT (QExpr PgExpressionSyntax s))
+allRuns = all_ (_runInfo cryptoDepthDb)
