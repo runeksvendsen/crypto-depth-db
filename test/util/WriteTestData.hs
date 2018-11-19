88cc20047cc2eee924a45b32c88b362ae7fde0ce
Rune K. Svendsen <runesvend@gmail.com>
2018-11-19 15:52:55 +0100

    WriteTestData: clean up imports

--- test/util/WriteTestData.hs
+++ test/util/WriteTestData.hs
@@ -1,35 +1,15 @@
 module Main where
 
-import           CryptoDepth.Db.Internal.Orphans
-import           Data.Proxy                     (Proxy(..))
-
-import qualified CryptoDepth
+import           CryptoDepth.Db.Internal.Orphans    ()
 import qualified CryptoDepth.Fetch              as Fetch
-import qualified OrderBook.Types                as OB
-
-import           CryptoVenues.Types.Market
-import           CryptoVenues.Fetch.MarketBook
 import qualified CryptoVenues.Types.AppM        as AppM
 
+import           Data.Proxy                     (Proxy(..))
 import qualified Network.HTTP.Client.TLS        as HTTPS
 import qualified Network.HTTP.Client            as HTTP
 import qualified Control.Logging                as Log
 import           Options.Applicative
 import qualified Data.Aeson                     as Json
-import           Data.Aeson                     ((.=), (.:))
-import           Data.Text.Lazy.IO              as L
-import           System.IO                      ( Handle, IOMode(WriteMode)
-                                                , withFile, stderr, hSetEncoding, utf8
-                                                )
-import Data.Text.Lazy.IO                        as L
-import qualified Money
--- TODO: move
-import GHC.TypeLits                             (KnownSymbol, SomeSymbol(..)
-                                                , symbolVal, someSymbolVal, sameSymbol
-                                                )
-import Data.Type.Equality                       ((:~:)(Refl))
-import Data.Maybe                               (fromMaybe)
-
 
 
 -- | In which currency do we want to measure liquidity?
