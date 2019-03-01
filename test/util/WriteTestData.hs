a362966cb6b1c67f2a2571fcde407292c55dabbb
Rune K. Svendsen <runesvend@gmail.com>
2019-03-01 15:08:54 +0100

    WriteTestData: fix for updated deps; take obLimit as arg

--- test/util/WriteTestData.hs
+++ test/util/WriteTestData.hs
@@ -1,7 +1,6 @@
 module Main where
 
-import           CryptoDepth.Db.Internal.Orphans    ()
-import qualified CryptoDepth.Fetch              as Fetch
+import qualified CryptoVenues.Fetch.Debug       as Fetch
 import qualified CryptoVenues.Types.AppM        as AppM
 
 import           Data.Proxy                     (Proxy(..))
@@ -10,28 +9,34 @@ import qualified Network.HTTP.Client            as HTTP
 import qualified Control.Logging                as Log
 import           Options.Applicative
 import qualified Data.Aeson                     as Json
+import           Control.Error                  (lefts, rights)
+import           Control.Monad                  (forM_)
 
 
 -- | In which currency do we want to measure liquidity?
 type Numeraire = "USD"
-
--- DEBUG: How many orderbooks to fetch from each venue
---  (not used in production)
-numObLimit :: Word
-numObLimit = 10000
 
 logLevel = Log.LevelInfo
 maxRetries = 15
 
 main :: IO ()
 main = do
-    targetFile <- execParser opts
+    args <- execParser opts
     man <- HTTP.newManager HTTPS.tlsManagerSettings
-    let throwErrM ioA = ioA >>= either (error . show) return
-    books <- throwErrM $ withLogging $ AppM.runAppM man maxRetries $
-        Fetch.allBooks (Proxy :: Proxy Numeraire) numObLimit
-    Json.encodeFile targetFile books
-    putStrLn $ "Wrote " ++ show targetFile
+    booksE <- throwErrM $ withLogging $ AppM.runAppM man maxRetries $
+        Fetch.allBooks (Proxy :: Proxy Numeraire) (obCount args)
+    -- Log errors
+    forM_ (lefts booksE) (\err -> putStrLn $ "Errors: " ++ show err)
+    -- Write JSON books
+    let books = rights booksE
+    if books /= []
+        then do
+            Json.encodeFile (targetFile args) books
+            putStrLn $ "Wrote " ++ show (targetFile args)
+        else
+            putStrLn $ "Fatal error: no order books fetched (all errored)"
+  where
+    throwErrM ioA = ioA >>= either (error . show) return
 
 withLogging :: IO a -> IO a
 withLogging ioa = Log.withStderrLogging $ do
@@ -39,14 +44,32 @@ withLogging ioa = Log.withStderrLogging $ do
     Log.setLogTimeFormat "%T.%3q"
     ioa
 
-targetFile :: Parser String
-targetFile = strOption
+opts :: ParserInfo Options
+opts = info options $
+     fullDesc
+  <> progDesc "Write test data to specified file"
+  <> header "Fetch & write test data"
+
+data Options = Options
+  { targetFile  :: FilePath
+  , obCount     :: Word
+  }
+
+options :: Parser Options
+options = Options
+      <$> targetFile'
+      <*> obCount'
+
+targetFile' :: Parser String
+targetFile' = strOption
   (  long "file"
   <> metavar "FILEPATH"
   <> help "File path to write test data to" )
 
-opts :: ParserInfo String
-opts = info targetFile $
-     fullDesc
-  <> progDesc "Write test data to specified file"
-  <> header "Fetch & write test data"
+obCount' :: Parser Word
+obCount' = option auto
+  ( long "ob-count"
+  <> short 'c'
+  <> value 100000
+  <> metavar "ORDERBOOK_COUNT"
+  <> help "Limit the number of fetched order books" )
